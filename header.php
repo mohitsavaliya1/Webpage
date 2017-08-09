ebaeb28be7161d22f73c4c6fc3a782d620ebeadd
Mohit Savaliya <mohitsavaliya111@gmail.com>
2017-08-10 00:46:14 +0530

    Add files via upload

---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+<title>Theme one rtcamp</title>
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+<body>
+
+<div class="container">
+	<div class="header col-md-12 col-md-offset-1">
+		<div class="row" style="background-image:url('<?php echo get_template_directory_uri(); ?>/Slices/header-bg.jpg');width: 100%" >
+		
+			<div class="logo">
+        		<img src="<?php echo get_template_directory_uri(); ?>/Slices/logo.png" />
+        	</div>
+
+			<div>   
+	        	<nav style="" >
+		        	<a href="#" >Home</a>
+			        <a href="#">News</a>
+			        <a href="#">Gallery</a>
+			        <a href="#">Pages</a>
+			        <a href="#">Layouts</a>
+			        <a href="#">Feauters</a>
+			        <a href="#">Blog</a>
+			        <a href="#">Contact Us</a>
+	      		</nav>
+	      	</div>
+    
+  		</div>
+    </div>
+</div>
+
+<!--
+<div class="bgimg">
+		<img src="<?php /*echo get_template_directory_uri(); */?>/Slices/header-bg.jpg" />
+</div>
+
+<div class="header">
+  		<img src="<?php /*echo get_template_directory_uri(); */?>/Slices/logo.png" />
+
+</div>
+-->
+
+     
